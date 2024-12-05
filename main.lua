d63f2800db58b15c384ca579ea1de72aa4afa9aa
art-muncher <doomanddesire@gmail.com>
2024-12-05 16:41:17 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -76,9 +76,84 @@ SMODS.Joker{
     calc_dollar_bonus = function(self,card)
         return 123
     end,
-  }
+}
+
+
+
+
+
+
+
+SMODS.ConsumableType{
+    key = 'DerekConsumableType', --consumable type key
+
+    collection_rows = {4,5}, --amount of cards in one page
+    primary_colour = G.C.PURPLE, --first color
+    secondary_colour = G.C.DARK_EDITION, --second color
+    loc_txt = {
+        collection = 'Derek Cards', --name displayed in collection
+        name = 'Derek', --name displayed in badge
+        undiscovered = {
+            name = 'Hidden Derek', --undiscovered name
+            text = {'Derek is', 'not here'} --undiscovered text
+        }
+    },
+    shop_rate = 1, --rate in shop out of 100
+}
+
+
+SMODS.UndiscoveredSprite{
+    key = 'DerekConsumableType', --must be the same key as the consumabletype
+    atlas = 'Jokers',
+    pos = {x = 0, y = 0}
+}
+
+
+SMODS.Consumable{
+    key = 'Derek', --key
+    set = 'DerekConsumableType', --the set of the card: corresponds to a consumable type
+    atlas = 'Jokers', --atlas
+    pos = {x = 0, y = 0}, --position in atlas
+    loc_txt = {
+        name = 'Derek Consumable', --name of card
+        text = { --text of card
+            'Add Negative to up to #1# selected cards',
+            'and create a Derek consumable'
+        }
+    },
+    config = {
+        extra = {
+            cards = 5, --configurable value
+        }
+    },
+    loc_vars = function(self,info_queue, center)
+        return {vars = {center.ability.extra.cards}} --displays configurable value: the #1# in the description is replaced with the configurable value
+    end,
+    can_use = function(self,card)
+        if G and G.hand then
+            if #G.hand.highlighted ~= 0 and #G.hand.highlighted <= card.ability.extra.cards then --if cards in hand highlighted are above 0 but below the configurable value then
+                return true
+            end
+        end
+        return false
+    end,
+    use = function(self,card,area,copier)
+        for i = 1, #G.hand.highlighted do 
+            --for every card in hand highlighted
+
+            G.hand.highlighted[i]:set_edition({negative = true},true)
+            --set their edition to negative
+        end
+
+        local newcard = create_card('DerekConsumableType', G.consumeables) --create a derek consumable
+        newcard:add_to_deck() --add it to deck
+        G.consumeables:emplace(newcard) --place it into G.consumeables
+
+    end,
+}
+
+
   
-  
-  ----------------------------------------------
-  ------------MOD CODE END----------------------
+----------------------------------------------
+------------MOD CODE END----------------------
     
